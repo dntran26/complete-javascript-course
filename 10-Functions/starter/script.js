3f9d8e66052ba43809c4d8fcb3fa833a093ab238
Danny Tran <dny.trn8@gmail.com>
2024-07-11 13:43:47 -0600

    finish 'Default Parameters' and start on 'How Passing Arguments Work'

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+/////////////////////////////////////////////////////////
+// How Passing Arguments Works: Value vs Reference
+
+/*
+/////////////////////////////////////////////////////////
+// Default Parameters
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassenger = 1,
+  price = 199 * numPassenger
+) {
+  // ES5
+  //   numPassenger = numPassenger || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
+*/
