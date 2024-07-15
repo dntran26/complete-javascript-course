8233714965d88a8fa309dfaa96f285bf8ade3b77
Danny Tran <dny.trn8@gmail.com>
2024-07-15 09:37:32 -0600

    finish 'Functions Accepting Callback Functions'

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -32,6 +32,7 @@ createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 */
 
+/*
 /////////////////////////////////////////////////////////
 // How Passing Arguments Works: Value vs Reference
 
@@ -66,3 +67,86 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+/*
+/////////////////////////////////////////////////////////
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('✋');
+};
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+// My own example of higher-order function
+
+const double = function (num) {
+  return num * 2;
+};
+
+const square = function (num) {
+  return num * num;
+};
+
+const mapArray = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i]));
+  }
+  return result;
+};
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(mapArray(numbers, double));
+console.log(mapArray(numbers, square));
+
+// Another example of higher-order function
+
+const upperCase = function (str) {
+  return str.toUpperCase();
+};
+
+const reverseStr = function (str) {
+  const splitString = str.split('');
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join('');
+  return joinArray;
+};
+
+const transformArray = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i]));
+  }
+  return result;
+};
+
+const words = ['hello', 'world', 'javascript', 'higher', 'order', 'functions'];
+
+console.log(transformArray(words, upperCase));
+console.log(transformArray(words, reverseStr));
+*/
