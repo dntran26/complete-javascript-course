e0155a1e0a1c8e685d8f33eb875dfb498f191722
Danny Tran <dny.trn8@gmail.com>
2024-07-16 09:18:57 -0600

    start on Coding Challenge #1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -278,3 +278,80 @@ const addGST2 = addTaxRate(0.05);
 console.log(addGST2(100));
 console.log(addGST2(23));
 */
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Danny Tran');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  reigsterNewAnswer() {
+    const answer = prompt(`What is your favourite programming language?
+  0: JavaScript
+  1: Python
+  2: Rust
+  3: C++
+  (Write option number)`);
+
+    if (answer === 0) {
+      this.answers[0].push({ answers });
+    } else if (answer === 1) {
+      this.answers[1]++;
+    } else if (answer === 2) {
+      this.answers[2]++;
+    } else if (answer === 3) {
+      this.answers[3]++;
+    }
+  },
+};
+
+poll.reigsterNewAnswer();
