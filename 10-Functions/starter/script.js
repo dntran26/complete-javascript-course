f537aca90212acae4fe6d39908b3fef9237720ee
Danny Tran <dny.trn8@gmail.com>
2024-08-14 16:00:31 -0600

    start Closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -374,6 +374,7 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 // [1, 5, 3, 9, 6, 1]
 */
 
+/*
 /////////////////////////////////////////////////////////
 // Immediately Invoked Function Expressions (IIFE)
 
@@ -399,3 +400,18 @@ runOnce();
 
 // console.log(isPrivate);
 console.log(notPrivate);
+*/
+
+/////////////////////////////////////////////////////////
+// Closures
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
